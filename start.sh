8a0ecd1c07b58208b090eb531c7612966ffaf89a
Michal Fojtak <mfojtak@seznam.cz>
2020-03-15 10:37:38 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,5 +8,5 @@
 #tensorboard --logdir /data/tb_logs &
 #cd theia
 #yarn theia start /data --hostname=0.0.0.0 &
-code-server --auth none /data /root &
+code-server --auth none -d $VSCODE_FOLDER $WORKSPACE_FOLDER &
 jupyter notebook --allow-root --no-browser --notebook-dir=$NB_DIR --ip 0.0.0.0 --NotebookApp.base_url=$NB_PREFIX $*
